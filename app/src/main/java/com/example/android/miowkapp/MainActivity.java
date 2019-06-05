312e03fd3386b79e0d01a2474160cd19b68905c4
shadow8evil@gmail.com <shadow8evil@gmail.com>
2019-06-05 08:59:33 +0300

    found a way to change the color for the taplayout looks good

--- app/src/main/java/com/example/android/miowkapp/MainActivity.java
+++ app/src/main/java/com/example/android/miowkapp/MainActivity.java
@@ -18,30 +18,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // //Set the content of the activity to use the activity_main.xml layout file
         // Find the view pager that will allow the user to swipe between fragments
-        // you can fine id.MyViewPager in activity main.xml under viewPager View
-
+        // you can fine id.viewpager in activity main.xml under viewPager View
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
         if (viewPager != null) {
             setupViewPager(viewPager);
         }
-        // Create an adapter that knows which fragment should be shown on each page
-        //  myViewPagerAdapter adapter = new myViewPagerAdapter(getSupportFragmentManager());
         //  add the icon to the tap layout
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
-
         tabLayout.setupWithViewPager(viewPager);
-
-
         //this code will change the color of the text on the tab layout both code below will work  just fine ..
-
-        //      tabLayout.setTabTextColors(Color.parseColor("#D3D3D3"),Color.parseColor("#f32159"));
-
-        tabLayout.setTabTextColors(getResources().getColor(R.color.colorAccent), getResources().getColor(R.color.myBlack));
-
+        //  tabLayout.setTabTextColors(Color.parseColor("#D3D3D3"),Color.parseColor("#f32159"));
+        tabLayout.setTabTextColors(getResources().getColor(R.color.myBlack), getResources().getColor(R.color.colorAccent));
         //  tabLayout.setTabIconTint(  );
-
 
 //        // to add icons to the taps
 //        tabLayout.getTabAt(0).setIcon(R.drawable.ic_colors);
@@ -49,20 +38,17 @@ public class MainActivity extends AppCompatActivity {
 //        tabLayout.getTabAt(2).setIcon(R.drawable.ic_numbers);
 //        tabLayout.getTabAt(3).setIcon(R.drawable.ic_proverb);
 
-
     } // end of onCreate method
-
 
     //  this method will add my fragments to the viewPager so i can see them on the screen
     private void setupViewPager(ViewPager viewPager) {
+        // Create an adapter that knows which fragment should be shown on each page
         myViewPagerAdapter adapter = new myViewPagerAdapter(getSupportFragmentManager());
+        //  the more you add the more taps you have but you need to create the class and xml file for the new fragments .
         adapter.addFragment(new Colors(), "Colors");
         adapter.addFragment(new Family(), "Family");
         adapter.addFragment(new Numbers(), "Numbers");
         adapter.addFragment(new phrases(), "Phrases");
-
-
-//  the more you add the more taps you have but you need to create the class and xml file for the new fragments .
         viewPager.setAdapter(adapter);
     }
 
