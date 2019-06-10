1d35eff4f1c9dbd5e2d79339b2a7c52129aa24b8
shadow8evil@gmail.com <shadow8evil@gmail.com>
2019-06-10 10:47:42 +0300

    added some comments to the word adapter and word model

--- app/src/main/java/com/example/android/miowkapp/WordsModel.java
+++ app/src/main/java/com/example/android/miowkapp/WordsModel.java
@@ -6,17 +6,38 @@ import android.widget.ListView;
 
 import java.util.ArrayList;
 
+
+/**
+ * {@link WordsModel} represent the vocabulary words that the user wants to learn .
+ * it also contains the translation for the words
+ */
 public class WordsModel {
-
-
     //declare private data instead of public to ensure the privacy of data field of each class
+
+    /**
+     * Default text in this case the English language  for each word
+     */
     private String mTextEnglish;
+    /**
+     * translation text in this case the translation  for each  English word
+     */
     private String mTextTranslation;
     private final static int Image = -1;
     private int mImageResource = Image;
     private int mAudio;
 
 
+    /**
+     * create a new WordModel  object
+     *
+     * @param mTextEnglish     the word  that the user is already familiar with
+     *                         ( such as English or any other languages ).
+     * @param mTextTranslation the word translation in arabic for example.
+     * @param mImageResource   the image which will be added next to each item in the list to help
+     *                         understanding the meaning of the word.
+     * @param mAudio           the audio which will be played to help the user understand the correct
+     *                         pronunciation.
+     */
     public WordsModel(String mTextEnglish, String mTextTranslation, int mImageResource, int mAudio) {
         this.mTextEnglish = mTextEnglish;
         this.mTextTranslation = mTextTranslation;
@@ -24,40 +45,66 @@ public class WordsModel {
         this.mAudio = mAudio;
     }
 
-
+    /**
+     * create a new WordModel  object
+     *
+     * @param mTextEnglish     the word  that the user is already familiar with
+     *                         ( such as English or any other languages ).
+     * @param mTextTranslation the word translation in arabic for example.
+     * @param mImageResource   the image which will be added next to each item in the list to help
+     *                         understanding the meaning of the word.
+     */
     public WordsModel(String mTextEnglish, String mTextTranslation, int mImageResource) {
         this.mTextEnglish = mTextEnglish;
         this.mTextTranslation = mTextTranslation;
         this.mImageResource = mImageResource;
     }
 
+    /**
+     * create a new WordModel  object
+     *
+     * @param mTextEnglish     the word  that the user is already familiar with
+     *                         ( such as English or any other languages ).
+     * @param mTextTranslation the word translation in arabic for example.
+     */
     public WordsModel(String mTextEnglish, String mTextTranslation) {
         this.mTextEnglish = mTextEnglish;
         this.mTextTranslation = mTextTranslation;
     }
 
-    // check wither an image is provided or not
+    /*
+    check wither an image is provided or not
+     */
     public boolean hasImage() {
         return mImageResource != Image;
     }
 
 
-    //retrieve the english text
+    /*
+    retrieve the english text
+     */
 
     public String getmTextEnglish() {
         return mTextEnglish;
     }
-    //retrieve the translation
 
+    /*
+    retrieve the translation
+     */
     public String getmTextTranslation() {
         return mTextTranslation;
     }
 
-    //retrieve the image id
+    /*
+     *     retrieve the image id
+     */
     public int getmImageResource() {
         return mImageResource;
     }
 
+    /*
+     * retrieve the audio for each word
+     */
     public int getmAudio() {
         return mAudio;
     }
@@ -65,11 +112,11 @@ public class WordsModel {
 
     public static ArrayList<WordsModel> getMyArrayListColors() {
         ArrayList<WordsModel> words = new ArrayList<WordsModel>();
-        words.add(new WordsModel("test Colors", "test 2 test 2" ,R.drawable.ic_colors));
+        words.add(new WordsModel("test Colors", "test 2 test 2", R.drawable.ic_colors));
         words.add(new WordsModel("test test", "test 2 test 2"));
         words.add(new WordsModel("test test", "test 2 test 2", R.drawable.ic_colors));
         words.add(new WordsModel("test test", "test 2 test 2"));
-        words.add(new WordsModel("test test", "test 2 test 2" , R.drawable.ic_colors));
+        words.add(new WordsModel("test test", "test 2 test 2", R.drawable.ic_colors));
 
         WordsModel wordsModel = new WordsModel("hi", "yo");
         words.add(wordsModel);
@@ -79,11 +126,11 @@ public class WordsModel {
     public static ArrayList<WordsModel> getArrayListFamily() {
 
         ArrayList<WordsModel> words = new ArrayList<WordsModel>();
-        words.add(new WordsModel("hello this Family fragment ", "fgfgfgfg" , R.drawable.ic_colors));
+        words.add(new WordsModel("hello this Family fragment ", "fgfgfgfg", R.drawable.ic_colors));
         words.add(new WordsModel("ghghghment ", "dfdfdfdf"));
         words.add(new WordsModel("hefgdfdfamily fragment ", "sdsdsdsd"));
         words.add(new WordsModel("heldfdfnt ", "3343434"));
-        words.add(new WordsModel("hello this Family fragment ", "54545454" , R.drawable.ic_house));
+        words.add(new WordsModel("hello this Family fragment ", "54545454", R.drawable.ic_house));
         words.add(new WordsModel("helldddfdfamily fragment ", "23232323"));
         words.add(new WordsModel("heldfdfragment ", "121212"));
         return words;
@@ -93,29 +140,29 @@ public class WordsModel {
 
     public static ArrayList<WordsModel> getArrayListNumbers() {
         ArrayList<WordsModel> words = new ArrayList<WordsModel>();
-        words.add(new WordsModel("Numbers", "اهلا" , R.drawable.ic_numbers));
+        words.add(new WordsModel("Numbers", "اهلا", R.drawable.ic_numbers));
         words.add(new WordsModel("hi", "اهلا"));
         words.add(new WordsModel("yo", "اهلا"));
         words.add(new WordsModel("qwqwqwqw", "اهلا"));
-        words.add(new WordsModel("wewew", "اهلا" , R.drawable.ic_people));
+        words.add(new WordsModel("wewew", "اهلا", R.drawable.ic_people));
         words.add(new WordsModel("woerererrds", "اهلا"));
         words.add(new WordsModel("rerer", "اهلا"));
         words.add(new WordsModel("rttrt", "اهلا"));
         words.add(new WordsModel("fgfgfgfg", "اهلا"));
-        words.add(new WordsModel("hhhhh", "اهلا" , R.drawable.ic_people2));
+        words.add(new WordsModel("hhhhh", "اهلا", R.drawable.ic_people2));
         words.add(new WordsModel("ggggg", "اهلا"));
         return words;
     }
 
     public static ArrayList<WordsModel> getArrayListPhrases() {
         ArrayList<WordsModel> words = new ArrayList<WordsModel>();
-        words.add(new WordsModel("phrases text",    "text" , R.drawable.ic_colors));
+        words.add(new WordsModel("phrases text", "text", R.drawable.ic_colors));
         words.add(new WordsModel("fgfgfgfg text", "ةيةيةية"));
         words.add(new WordsModel("dfdfdfdf text", "ةيةيةية"));
         words.add(new WordsModel("phrases ssdsdsd", "ةيةيةية"));
         words.add(new WordsModel("phrases text", "ةيةيةية"));
         words.add(new WordsModel("45454545 text", "ةيةيةية"));
-        words.add(new WordsModel("454545 text",  "text" , R.drawable.ic_pen));
+        words.add(new WordsModel("454545 text", "text", R.drawable.ic_pen));
         return words;
     }
 
--- app/src/main/java/com/example/android/miowkapp/myWordAdapter.java
+++ app/src/main/java/com/example/android/miowkapp/myWordAdapter.java
@@ -14,39 +14,77 @@ import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
+
+/**
+ * {@link myWordAdapter} is an {@link ArrayAdapter} that can provide the layout for each list
+ * based on a data source, which is a list of {@link WordsModel} objects.
+ */
 public class myWordAdapter extends ArrayAdapter<WordsModel> {
 
-
+    /**
+     * This is our own custom constructor (it doesn't mirror a superclass constructor).
+     * The context is used to inflate the layout file, and the list is the data we want
+     * to populate into the lists.
+     *
+     * @param context        The current context. Used to inflate the layout file.
+     * @param wordsModels A List of AndroidFlavor objects to display in a list
+     */
     public myWordAdapter(Context context, ArrayList<WordsModel> wordsModels) {
+        // Here, we initialize the ArrayAdapter's internal storage for the context and the list.
+        // the second argument is used when the ArrayAdapter is populating a single TextView.
+        // Because this is a custom adapter for two TextViews and an ImageView, the adapter is not
+        // going to use this second argument, so it can be any value. Here, we used 0.
         super(context, 0, wordsModels);
     }
 
-
+    /**
+     * Provides a view for an AdapterView (ListView, GridView, etc.)
+     *
+     * @param position The position in the list of data that should be displayed in the
+     *                 list item view.
+     * @param convertView The recycled view to populate.
+     * @param parent The parent ViewGroup that is used for inflation.
+     * @return The View for the position in the AdapterView.
+     */
     @NonNull
     @Override
     public View getView(int position, View convertView, @NonNull ViewGroup parent) {
+
+        // Check if the existing view is being reused, otherwise inflate the view
+
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.custom_list_word_layout, parent, false);
         }
 
-
+        // Get the {@link WordModel} object located at this position in the list
         WordsModel word = getItem(position);
 
+        // Find the TextView in the custom_list_word_layout.xml layout with the ID EnglishText
         TextView textEnglish = (TextView) convertView.findViewById(R.id.EnglishText);
+        // Find the TextView in the custom_list_word_layout.xml layout with the ID TranslationText
         TextView textTranslation = (TextView) convertView.findViewById(R.id.TranslationText);
+        // Find the TextView in the custom_list_word_layout.xml layout with the ID imageView
         ImageView imageView = (ImageView) convertView.findViewById(R.id.imageView);
 
+        // Get the version name from the current WordModel object : word
+        // set this text on the textEnglish TextView
         textEnglish.setText(word.getmTextEnglish());
+
+        // Get the version name from the current WordModel object : word
+        // set this text on the textTranslation TextView
         textTranslation.setText(word.getmTextTranslation());
 
         if (word.hasImage()) {
             imageView.setVisibility(View.VISIBLE);
+            // Get the image resource ID from the current AndroidFlavor object and
+            // set the image to iconView
             imageView.setImageResource(word.getmImageResource());
         } else {
             imageView.setVisibility(View.GONE);
         }
 
-
+        // Return the whole list item layout (containing 2 TextViews and an ImageView)
+        // so that it can be shown in the ListView
         return convertView;
     }// end of view method
 
