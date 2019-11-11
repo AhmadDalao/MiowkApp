a295953857ec90fff32deac34ffe2cb0d0cab3e5
shadow8evil@gmail.com <shadow8evil@gmail.com>
2019-11-11 17:34:25 +0300

    nothing

--- app/src/main/java/com/example/android/miowkapp/WordsModel.java
+++ app/src/main/java/com/example/android/miowkapp/WordsModel.java
@@ -125,16 +125,15 @@ public class WordsModel {
      */
     public static ArrayList<WordsModel> getMyArrayListColors() {
         // Create an ArrayList of WordsModel objects
-
         ArrayList<WordsModel> words = new ArrayList<WordsModel>();
-        words.add(new WordsModel("black", "test 2 test 2", R.drawable.color_black, R.raw.color_black));
-        words.add(new WordsModel("brown", "test 2 test 2", R.drawable.color_brown, R.raw.color_brown));
-        words.add(new WordsModel("dusty yellow", "test 2 test 2", R.drawable.color_dusty_yellow, R.raw.color_dusty_yellow));
-        words.add(new WordsModel("gray", "test 2 test 2", R.drawable.color_gray, R.raw.color_gray));
-        words.add(new WordsModel("green", "test 2 test 2", R.drawable.color_green, R.raw.color_green));
-        words.add(new WordsModel("mustard yellow", "test 2 test 2", R.drawable.color_mustard_yellow, R.raw.color_mustard_yellow));
-        words.add(new WordsModel("red", "test 2 test 2", R.drawable.color_red, R.raw.color_red));
-        words.add(new WordsModel("white", "test 2 test 2"));
+        words.add(new WordsModel("red", "weṭeṭṭi", R.drawable.color_red, R.raw.color_red));
+        words.add(new WordsModel("green", "chokokki", R.drawable.color_green, R.raw.color_green));
+        words.add(new WordsModel("brown", "ṭakaakki", R.drawable.color_brown, R.raw.color_brown));
+        words.add(new WordsModel("gray", "ṭopoppi", R.drawable.color_gray, R.raw.color_gray));
+        words.add(new WordsModel("black", "kululli", R.drawable.color_black, R.raw.color_black));
+        words.add(new WordsModel("white", "kelelli", R.drawable.color_white, R.raw.color_white));
+        words.add(new WordsModel("dusty yellow", "ṭopiisә", R.drawable.color_dusty_yellow, R.raw.color_dusty_yellow));
+        words.add(new WordsModel("mustard yellow", "chiwiiṭә", R.drawable.color_mustard_yellow, R.raw.color_mustard_yellow));
         return words;
     }
 
@@ -146,13 +145,16 @@ public class WordsModel {
         // Create an ArrayList of WordsModel objects
 
         ArrayList<WordsModel> words = new ArrayList<WordsModel>();
-        words.add(new WordsModel("father ", "fgfgfgfg", R.drawable.family_father, R.raw.family_father));
-        words.add(new WordsModel("mother ", "dfdfdfdf", R.drawable.family_mother, R.raw.family_mother));
-        words.add(new WordsModel("older sister ", "sdsdsdsd", R.drawable.family_older_sister, R.raw.family_older_sister));
-        words.add(new WordsModel("older brother ", "3343434", R.drawable.family_older_brother, R.raw.family_older_brother));
-        words.add(new WordsModel("grandfather ", "54545454", R.drawable.family_grandfather, R.raw.family_grandfather));
-        words.add(new WordsModel("grandmother ", "23232323", R.drawable.family_grandmother, R.raw.family_grandmother));
-        words.add(new WordsModel("son ", "121212", R.drawable.family_son, R.raw.family_son));
+        words.add(new WordsModel("father", "әpә", R.drawable.family_father, R.raw.family_father));
+        words.add(new WordsModel("mother", "әṭa",R.drawable.family_mother, R.raw.family_mother));
+        words.add(new WordsModel("son", "angsi", R.drawable.family_son, R.raw.family_son));
+        words.add(new WordsModel("daughter", "tune", R.drawable.family_daughter, R.raw.family_daughter));
+        words.add(new WordsModel("older brother", "taachi", R.drawable.family_older_brother, R.raw.family_older_brother));
+        words.add(new WordsModel("younger brother", "chalitti", R.drawable.family_younger_brother, R.raw.family_younger_brother));
+        words.add(new WordsModel("older sister", "teṭe", R.drawable.family_older_sister, R.raw.family_older_sister));
+        words.add(new WordsModel("younger sister", "kolliti", R.drawable.family_younger_sister, R.raw.family_younger_sister));
+        words.add(new WordsModel("grandmother", "ama", R.drawable.family_grandmother, R.raw.family_grandmother));
+        words.add(new WordsModel("grandfather", "paapa", R.drawable.family_grandfather, R.raw.family_grandfather));
         return words;
 
     }
@@ -164,16 +166,17 @@ public class WordsModel {
         // Create an ArrayList of WordsModel objects
 
         ArrayList<WordsModel> words = new ArrayList<WordsModel>();
-        words.add(new WordsModel("one", "اهلا", R.drawable.number_one, R.raw.number_one));
-        words.add(new WordsModel("two", "اهلا", R.drawable.number_two, R.raw.number_two));
-        words.add(new WordsModel("three", "hi", R.drawable.number_three, R.raw.number_three));
-        words.add(new WordsModel("four", "h", R.drawable.number_four, R.raw.number_four));
-        words.add(new WordsModel("five", "hh", R.drawable.number_five, R.raw.number_five));
-        words.add(new WordsModel("six", "hhh", R.drawable.number_six, R.raw.number_six));
-        words.add(new WordsModel("seven", "gghg", R.drawable.number_seven, R.raw.number_seven));
-        words.add(new WordsModel("eight", "fgfg", R.drawable.number_eight, R.raw.number_eight));
-        words.add(new WordsModel("nine", "dfdfdf", R.drawable.number_nine, R.raw.number_nine));
-        words.add(new WordsModel("ten", "ssd", R.drawable.number_ten, R.raw.number_ten));
+        words.add(new WordsModel("one", "lutti", R.drawable.number_one, R.raw.number_one));
+        words.add(new WordsModel("two", "ottiko", R.drawable.number_two, R.raw.number_two));
+        words.add(new WordsModel("three", "tolookosu", R.drawable.number_three, R.raw.number_three));
+        words.add(new WordsModel("four", "oyyisa", R.drawable.number_four, R.raw.number_four));
+        words.add(new WordsModel("five", "massoka", R.drawable.number_five, R.raw.number_five));
+        words.add(new WordsModel("six", "temmoka", R.drawable.number_six, R.raw.number_six));
+        words.add(new WordsModel("seven", "kennekaku", R.drawable.number_seven, R.raw.number_seven));
+        words.add(new WordsModel("eight", "kawinta", R.drawable.number_eight, R.raw.number_eight));
+        words.add(new WordsModel("nine", "wo'e", R.drawable.number_nine, R.raw.number_nine));
+        words.add(new WordsModel("ten", "na'accha", R.drawable.number_ten, R.raw.number_ten));
+
         return words;
     }
 
@@ -186,13 +189,16 @@ public class WordsModel {
         // Create an ArrayList of WordsModel objects
 
         ArrayList<WordsModel> words = new ArrayList<WordsModel>();
-        words.add(new WordsModel("phrase are you coming", "text", R.raw.phrase_are_you_coming));
-        words.add(new WordsModel("phrase_come_here", "ةيةيةية", R.raw.phrase_come_here));
-        words.add(new WordsModel("phrase how are you feeling ", "ةيةيةية", R.raw.phrase_how_are_you_feeling));
-        words.add(new WordsModel("im coming", "ةيةيةية", R.raw.phrase_im_coming));
-        words.add(new WordsModel("im feeling good", "ةيةيةية", R.raw.phrase_im_feeling_good));
-        words.add(new WordsModel("what is your name", "ةيةيةية", R.raw.phrase_what_is_your_name));
-        words.add(new WordsModel("my name is ", "text", R.raw.phrase_my_name_is));
+        words.add(new WordsModel("Where are you going?", "minto wuksus", R.raw.phrase_where_are_you_going));
+        words.add(new WordsModel("What is your name?", "tinnә oyaase'nә", R.raw.phrase_what_is_your_name));
+        words.add(new WordsModel("My name is...", "oyaaset...", R.raw.phrase_my_name_is));
+        words.add(new WordsModel("How are you feeling?", "michәksәs?", R.raw.phrase_how_are_you_feeling));
+        words.add(new WordsModel("I’m feeling good.", "kuchi achit", R.raw.phrase_im_feeling_good));
+        words.add(new WordsModel("Are you coming?", "әәnәs'aa?", R.raw.phrase_are_you_coming));
+        words.add(new WordsModel("Yes, I’m coming.", "hәә’ әәnәm", R.raw.phrase_yes_im_coming));
+        words.add(new WordsModel("I’m coming.", "әәnәm", R.raw.phrase_im_coming));
+        words.add(new WordsModel("Let’s go.", "yoowutis", R.raw.phrase_lets_go));
+        words.add(new WordsModel("Come here.", "әnni'nem", R.raw.phrase_come_here));
         return words;
     }
 
